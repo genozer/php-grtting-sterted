da8b9ec58d31235295a40906f100e410ef1f7a44
genozer <42301010+genozer@users.noreply.github.com>
2018-08-17 01:38:45 +0700

    Update index.php

--- index.php
+++ index.php
@@ -45,4 +45,160 @@ curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 $result = curl_exec($ch);
 curl_close ($ch);
  
+public function handle()
+    {
+        $TEACH_SIGN = '==';
+        $text = $this->textMessage->getText();
+        $text = trim(text);
+        # Remove ZWSP
+        $replyToken = $this->textMessage->getReplyToken();
+        $this->logger->info("Got text message from $replyToken: $text");
+
+        switch ($text) {
+            case 'profile':
+                $userId = $this->textMessage->getUserId();
+                $this->sendProfile($replyToken, $userId);
+                break;
+            case 'bye':
+                if ($this->textMessage->isRoomEvent()) {
+                    $this->bot->replyText($replyToken, 'Leaving room');
+                    $this->bot->leaveRoom($this->textMessage->getRoomId());
+                    break;
+                }
+                if ($this->textMessage->isGroupEvent()) {
+                    $this->bot->replyText($replyToken, 'Leaving group');
+                    $this->bot->leaveGroup($this->textMessage->getGroupId());
+                    break;
+                }
+                $this->bot->replyText($replyToken, 'Bot cannot leave from 1:1 chat');
+                break;
+            case 'confirm':
+                $this->bot->replyMessage(
+                    $replyToken,
+                    new TemplateMessageBuilder(
+                        'Confirm alt text',
+                        new ConfirmTemplateBuilder('Do it?', [
+                            new MessageTemplateActionBuilder('Yes', 'Yes!'),
+                            new MessageTemplateActionBuilder('No', 'No!'),
+                        ])
+                    )
+                );
+                break;
+            case 'buttons':
+                $imageUrl = UrlBuilder::buildUrl($this->req, ['static', 'buttons', '1040.jpg']);
+                $buttonTemplateBuilder = new ButtonTemplateBuilder(
+                    'My button sample',
+                    'Hello my button',
+                    $imageUrl,
+                    [
+                        new UriTemplateActionBuilder('Go to line.me', 'https://line.me'),
+                        new PostbackTemplateActionBuilder('Buy', 'action=buy&itemid=123'),
+                        new PostbackTemplateActionBuilder('Add to cart', 'action=add&itemid=123'),
+                        new MessageTemplateActionBuilder('Say message', 'hello hello'),
+                    ]
+                );
+                $templateMessage = new TemplateMessageBuilder('Button alt text', $buttonTemplateBuilder);
+                $this->bot->replyMessage($replyToken, $templateMessage);
+                break;
+            case 'carousel':
+                $imageUrl = UrlBuilder::buildUrl($this->req, ['static', 'buttons', '1040.jpg']);
+                $carouselTemplateBuilder = new CarouselTemplateBuilder([
+                    new CarouselColumnTemplateBuilder('foo', 'bar', $imageUrl, [
+                        new UriTemplateActionBuilder('Go to line.me', 'https://line.me'),
+                        new PostbackTemplateActionBuilder('Buy', 'action=buy&itemid=123'),
+                    ]),
+                    new CarouselColumnTemplateBuilder('buz', 'qux', $imageUrl, [
+                        new PostbackTemplateActionBuilder('Add to cart', 'action=add&itemid=123'),
+                        new MessageTemplateActionBuilder('Say message', 'hello hello'),
+                    ]),
+                ]);
+                $templateMessage = new TemplateMessageBuilder('Button alt text', $carouselTemplateBuilder);
+                $this->bot->replyMessage($replyToken, $templateMessage);
+                break;
+            case 'imagemap':
+                $richMessageUrl = UrlBuilder::buildUrl($this->req, ['static', 'rich']);
+                $imagemapMessageBuilder = new ImagemapMessageBuilder(
+                    $richMessageUrl,
+                    'This is alt text',
+                    new BaseSizeBuilder(1040, 1040),
+                    [
+                        new ImagemapUriActionBuilder(
+                            'https://store.line.me/family/manga/en',
+                            new AreaBuilder(0, 0, 520, 520)
+                        ),
+                        new ImagemapUriActionBuilder(
+                            'https://store.line.me/family/music/en',
+                            new AreaBuilder(520, 0, 520, 520)
+                        ),
+                        new ImagemapUriActionBuilder(
+                            'https://store.line.me/family/play/en',
+                            new AreaBuilder(0, 520, 520, 520)
+                        ),
+                        new ImagemapMessageActionBuilder(
+                            'URANAI!',
+                            new AreaBuilder(520, 520, 520, 520)
+                        )
+                    ]
+                );
+                $this->bot->replyMessage($replyToken, $imagemapMessageBuilder);
+                break;
+            case 'quickReply':
+                $postback = new PostbackTemplateActionBuilder('Buy', 'action=quickBuy&itemid=222', 'Buy');
+                $datetimePicker = new DatetimePickerTemplateActionBuilder(
+                    'Select date',
+                    'storeId=12345',
+                    'datetime',
+                    '2017-12-25t00:00',
+                    '2018-01-24t23:59',
+                    '2017-12-25t00:00'
+                );
+
+                $quickReply = new QuickReplyMessageBuilder([
+                    new QuickReplyButtonBuilder(new LocationTemplateActionBuilder('Location')),
+                    new QuickReplyButtonBuilder(new CameraTemplateActionBuilder('Camera')),
+                    new QuickReplyButtonBuilder(new CameraRollTemplateActionBuilder('Camera roll')),
+                    new QuickReplyButtonBuilder($postback),
+                    new QuickReplyButtonBuilder($datetimePicker),
+                ]);
+
+                $messageTemplate = new TextMessageBuilder('Text with quickReply buttons', $quickReply);
+                $this->bot->replyMessage($replyToken, $messageTemplate);
+                break;
+            default:
+                $this->echoBack($replyToken, $text);
+                break;
+        }
+    }
+
+    /**
+     * @param string $replyToken
+     * @param string $text
+     */
+    private function echoBack($replyToken, $text)
+    {
+        $this->logger->info("Returns echo message $replyToken: $text");
+        $this->bot->replyText($replyToken, $text);
+    }
+
+    private function sendProfile($replyToken, $userId)
+    {
+        if (!isset($userId)) {
+            $this->bot->replyText($replyToken, "Bot can't use profile API without user ID");
+            return;
+        }
+
+        $response = $this->bot->getProfile($userId);
+        if (!$response->isSucceeded()) {
+            $this->bot->replyText($replyToken, $response->getRawBody());
+            return;
+        }
+
+        $profile = $response->getJSONDecodedBody();
+        $this->bot->replyText(
+            $replyToken,
+            'Display name: ' . $profile['displayName'],
+            'Status message: ' . $profile['statusMessage']
+        );
+    }
+}
 ?>
